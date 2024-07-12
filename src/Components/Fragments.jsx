2df2a06e577bbf8dc89d8913b73e548d26f10f31
ChrisChris501 <Christanahagbekeye@gmail.com>
2024-07-12 13:39:38 +0100

    React Fragments

--- src/Components/Fragments.jsx
+++ src/Components/Fragments.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Fragments = () => {
   return (
-      <>
-      <h1>Welcome to My Website</h1>
-      <p>Your one-stop solution for all your needs.</p>
-    </>
+      <React.Fragment> {/* Shorthand: `<>` `</>` */}
+        <h1>Welcome to My Website</h1>
+        <p>Your one-stop solution for all your needs.</p>
+      </React.Fragment> 
   )
 }
 
